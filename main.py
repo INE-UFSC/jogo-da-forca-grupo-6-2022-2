68fbe2aac9032150c876f310b1b6cde04926f116
jresmer <jpresmer@gmail.com>
2022-08-30 11:25:39 -0300

    adicionada a lista de frases prontas

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 matriz_mapeamento = ["no"]*26
-frase = input()
+lista_de_frases = ["bola de bilhar", "orientacao a objetos", "manipulacao dos dados", "projeto final", "spaghetti code", "palavra", "teste", "debug", "correcao"]
+opcao = input("digite '1' para adicionar uma frase ou palavra a lista ou '2' para continuar:")
+if opcao == '1':
+    continuar = True
+    while continuar:
+        print("são caracteres aceitos apenas as 26 letras do alfabeto e espaços")
+        nova_frase = input("digite a nova frase/palavra:")
+        continuar_checagem = input("digite '1' para continuar a adicionar plavras/frases ou '2' para encerrar esse processo")
+        if continuar_checagem == '2':
+            continuar = False
+frase = input("digite um número entre 0 e %d para selecionar a palavra/frase." % len(lista_de_frases))
+frase = lista_de_frases[int(frase)]
 numero_de_caracteres = len(frase)
 for letra_pos in range (numero_de_caracteres):
     valor_ascii = ord(frase[letra_pos].lower()) - 97
