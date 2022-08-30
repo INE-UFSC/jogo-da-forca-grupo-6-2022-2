45871a6269d5b06335dcf1cc1f3867efd83711fe
jresmer <jpresmer@gmail.com>
2022-08-30 10:09:39 -0300

    corrigido o if

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ frase = input()
 numero_de_caracteres = len(frase)
 for letra_pos in range (numero_de_caracteres):
     valor_ascii = ord(frase[letra_pos].lower()) - 97
-    if frase[letra_pos] = " ":
+    if frase[letra_pos] == " ":
         continue
     elif matriz_mapeamento[valor_ascii] == "no":
         matriz_mapeamento[valor_ascii] = [letra_pos]
     else:
         matriz_mapeamento[valor_ascii] += [letra_pos]
+
+print(matriz_mapeamento)
